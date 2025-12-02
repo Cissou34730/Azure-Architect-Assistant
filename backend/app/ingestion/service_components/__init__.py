00d2d99a98ba13467b71d8aaac3344cff67f2ae9
Cissou34730 <cyril.beurier@yahoo.fr>
2025-12-02 17:11:08 +0100

    Remove legacy code

--- backend/app/ingestion/service_components/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Component modules backing the ingestion service manager."""
-
-from .manager import IngestionService
-from .state import IngestionState
-
-__all__ = ["IngestionService", "IngestionState"]
